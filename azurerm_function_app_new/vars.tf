9d751419882a0803a06754792b99bb4d2384f62a
Pasquale De Vita <pasqualedevita@Pasquales-MacBook-Pro.local>
2020-06-09 13:18:48 +0200

    fixed secrets

--- azurerm_function_app_new/vars.tf
+++ azurerm_function_app_new/vars.tf
@@ -85,13 +85,6 @@ variable app_settings {
   default = {}
 }
 
-variable app_settings_secrets {
-  type = object({
-    key_vault_id = string
-    map          = map(string)
-  })
-}
-
 variable "allowed_ips" {
   // List of ip
   type    = list(string)
