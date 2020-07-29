9db91d7d2161d28a15a68602bf40f6afe1840034
Pasquale De Vita <pasqualedevita@Pasquales-MacBook-Pro.local>
2020-07-29 20:01:06 +0200

    removed workflow schema version

--- azurerm_logic_app/vars.tf
+++ azurerm_logic_app/vars.tf
@@ -22,16 +22,6 @@ variable "resource_group_name" {
   type = string
 }
 
-variable "workflow_schema" {
-  type    = string
-  default = null
-}
-
-variable "workflow_version" {
-  type    = string
-  default = null
-}
-
 locals {
   resource_name = "${var.global_prefix}-${var.environment_short}-lapp-${var.name}"
 }
