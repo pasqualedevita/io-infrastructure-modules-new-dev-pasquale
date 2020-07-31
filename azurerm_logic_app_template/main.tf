a89d03331cd740706ae4a6864cfb4fdd467bb7c0
Pasquale De Vita <pasqualedevita@Pasquales-MacBook-Pro.local>
2020-07-31 14:10:34 +0200

    arm template test

---
+++ azurerm_logic_app_template/main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  version = "=2.11.0"
+  features {}
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "azurerm" {}
+}
+
+resource "azurerm_logic_app_workflow" "logic_app" {
+  name                = local.resource_name
+  resource_group_name = var.resource_group_name
+  location            = var.region
+  workflow_schema     = var.workflow_schema
+}
